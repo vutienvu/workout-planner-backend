3d4456bc2a1cbe58397e83478e994cbb79e48ded
Tien Vuong Vu <vutienvu@cvut.cz>
2024-07-25 18:51:53 +0200

    [feat]: Add database context into DI container

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using WorkoutPlanner.Helper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    options.UseSnakeCaseNamingConvention();
+});
 
 var app = builder.Build();
 
