f3a48bc9384773a3278628385dbeba19abbf5fe9
Tien Vuong Vu <vutienvu@cvut.cz>
2024-07-26 17:22:16 +0200

    [build]: Initial database migration

---
+++ Migrations/20240726152000_InitializingDatabase.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutPlanner.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializingDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "workouts",
+                columns: table => new
+                {
+                    workout_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_workouts", x => x.workout_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "exercises",
+                columns: table => new
+                {
+                    exercise_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    pause_duration = table.Column<int>(type: "int", nullable: false),
+                    workout_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_exercises", x => x.exercise_id);
+                    table.ForeignKey(
+                        name: "fk_exercises_workouts_workout_id",
+                        column: x => x.workout_id,
+                        principalTable: "workouts",
+                        principalColumn: "workout_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "exercise_terms",
+                columns: table => new
+                {
+                    exercise_term_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    term_date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    total_sets = table.Column<int>(type: "int", nullable: false),
+                    exercise_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_exercise_terms", x => x.exercise_term_id);
+                    table.ForeignKey(
+                        name: "fk_exercise_terms_exercises_exercise_id",
+                        column: x => x.exercise_id,
+                        principalTable: "exercises",
+                        principalColumn: "exercise_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "sets",
+                columns: table => new
+                {
+                    set_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    reps = table.Column<int>(type: "int", nullable: false),
+                    weight = table.Column<float>(type: "float", nullable: false),
+                    reps_type = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    weight_type = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    exercise_term_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_sets", x => x.set_id);
+                    table.ForeignKey(
+                        name: "fk_sets_exercise_terms_exercise_term_id",
+                        column: x => x.exercise_term_id,
+                        principalTable: "exercise_terms",
+                        principalColumn: "exercise_term_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_exercise_terms_exercise_id",
+                table: "exercise_terms",
+                column: "exercise_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_exercises_workout_id",
+                table: "exercises",
+                column: "workout_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_sets_exercise_term_id",
+                table: "sets",
+                column: "exercise_term_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "sets");
+
+            migrationBuilder.DropTable(
+                name: "exercise_terms");
+
+            migrationBuilder.DropTable(
+                name: "exercises");
+
+            migrationBuilder.DropTable(
+                name: "workouts");
+        }
+    }
+}
